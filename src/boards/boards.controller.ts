794a4cb0c8564925d1b0a76b6eb56f13c8dd9463
SugarSyrup <tlfvm04@naver.com>
2023-07-12 20:31:31 +0900

    FEAT: CRUD Board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete, Patch } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
 
+    @Get()
     getAllBoards(): Board[] {
         return this.boardsService.getAllBoards();
+    }
+
+    @Get("/:id")
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
+    @Post()
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Delete("/:id")
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch("/:id/status")
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.boardsService.updateBoard(id, status);
     }
 }
